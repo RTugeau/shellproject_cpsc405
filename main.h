1d17aa9610d815196bac6fa8eb0325564c6cba3e
Ethan Thompson <ethan.m.thompson@gmail.com>
2023-03-14 23:09:23 +0000

    Added main.h and edited gitignore

---
+++ main.h
@@ -0,0 +1,10 @@
+
+
+//definitions for functions here.
+
+void parseCmd(char* cmd, char** params, int *nparams);
+int executeCmd(char** params, int nparams);
+int pwd();
+int catHandler(char* input);
+int dogHandler(char* input);
+int ls(char* input);
